bd2ae9b04cdb9dbab2f8775443a196fc398e8778
niyozneo <niyoz2005@gmail.com>
2024-06-04 12:06:28 +0500

    some fixes for file uplaoad

--- src/modules/file-upload/file-upload.service.ts
+++ src/modules/file-upload/file-upload.service.ts
@@ -1,7 +1,12 @@
 import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import * as fs from 'fs'; // Import the 'fs' module for file system operations
 import * as mime from 'mime-types';
+
 @Injectable()
 export class FileUploadService {
+  // Define the upload destination folder (adjust the path as needed)
+  private readonly uploadPath = process.cwd()+ '/media';
+
   async uploadImage(file: Express.Multer.File): Promise<string> {
     try {
       const fileExtension = mime.extension(file.mimetype);
@@ -12,12 +17,25 @@ export class FileUploadService {
       const randomName: string =
         new Date().getTime().toString() + '.' + fileExtension;
 
-      return randomName;
+      // Create the upload directory if it doesn't exist
+      if (!fs.existsSync(this.uploadPath)) {
+        fs.mkdirSync(this.uploadPath);
+      }
+
+      // Build the complete file path
+      const filePath = `${this.uploadPath}/${randomName}`;
+      console.log(filePath);
+      
+      // Write the file to the upload directory
+      const s =await fs.promises.writeFile(filePath, file.buffer);
+      console.log(s);
+      
+      return randomName; // Return the generated filename
     } catch (error) {
       Logger.error('Error during file upload:', error);
       throw new HttpException(
         'File upload failed',
-        HttpStatus.INTERNAL_SERVER_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR
       );
     }
   }
