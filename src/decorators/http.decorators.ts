b97af664c10d0414d3fa07b58f403c31e6b9b96e
MahdiyDev <mahdiydev@gmail.com>
2024-06-08 19:39:21 +0500

    Added: premises cherry-pick functionality

--- src/decorators/http.decorators.ts
+++ src/decorators/http.decorators.ts
@@ -1,5 +1,6 @@
 import {
 	Param,
+	ParseArrayPipe,
 	ParseUUIDPipe,
 	Query,
 	UseGuards,
@@ -65,6 +66,29 @@ export function UUIDQuery(
 ): ParameterDecorator {
 	return Query(
 		property,
+		new ParseUUIDPipe({
+			optional: isOptinal,
+			version: "4",
+			exceptionFactory(error_string) {
+				const error = new ValidationError();
+				error.property = property;
+				error.target = { ParseUUIDPipe: error_string };
+				error.constraints = { ParseUUIDPipe: error_string };
+				return new CustomValidationError([error]);
+			},
+		}),
+		...pipes,
+	);
+}
+
+export function UUIDArrayQuery(
+	property: string,
+	isOptinal = false,
+	...pipes: Array<Type<PipeTransform> | PipeTransform>
+): ParameterDecorator {
+	return Query(
+		property,
+		new ParseArrayPipe({ optional: isOptinal }),
 		new ParseUUIDPipe({
 			optional: isOptinal,
 			version: "4",
