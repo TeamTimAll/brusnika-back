96ce499a7c4596d1596ea1971fbfef1a18a8b114
MahdiyDev <mahdiydev@gmail.com>
2024-07-03 15:36:19 +0500

    Fixed: filter by changing IsNotEmpty to IsOptional

--- src/modules/bookings/dtos/NotBookedPremisesFilter.dto.ts
+++ src/modules/bookings/dtos/NotBookedPremisesFilter.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum, IsNotEmpty, IsUUID } from "class-validator";
+import { IsEnum, IsNotEmpty, IsOptional, IsUUID } from "class-validator";
 
 import { PremisesType } from "../../leads/lead_ops.entity";
 
@@ -8,7 +8,7 @@ export class NotBookedPremisesFilter {
 		enum: PremisesType,
 		description: "Premises type",
 	})
-	@IsNotEmpty()
+	@IsOptional()
 	@IsEnum(PremisesType)
 	type?: PremisesType;
 
