7cb1e089733acdc1917e4e2675e0512791e60844
MahdiyDev <mahdiydev@gmail.com>
2024-07-17 15:40:31 +0500

    Added: user roles

--- src/constants/role-type.ts
+++ src/constants/role-type.ts
@@ -1,7 +1,11 @@
 export enum RoleType {
-	USER = "USER",
-	ADMIN = "ADMIN",
-	AGENT = "AGENT",
-	MANAGER = "MANAGER",
-	EMPLOYEE = "EMPLOYEE",
+	USER              = "USER",               // Пользователь
+	ADMIN             = "ADMIN",              // Администрация
+	AGENT             = "AGENT",              // Агент
+	MANAGER           = "MANAGER",            // Менеджер
+	EMPLOYEE          = "EMPLOYEE",           // Cотрудник
+	NEW_MEMBER        = "NEW_MEMBER",         // Новый участник
+	HEAD_OF_AGENCY    = "HEAD_OF_AGENCY",     // Руководитель агентства
+	OZK_MANAGER       = "OZK_MANAGER",        // Менеджер
+	AFFILIATE_MANAGER = "AFFILIATE_MANAGER",  // Партнерский менеджер
 }
