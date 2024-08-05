32d2afc30dbc18a546687fca7472110acf45bdbc
MahdiyDev <mahdiydev@gmail.com>
2024-08-05 11:03:45 +0500

    Added: city

--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -14,6 +14,7 @@ import { UserService } from "../../modules/user/user.service";
 import { ServiceResponse } from "../../types";
 import { AgencyEntity } from "../agencies/agencies.entity";
 import { UserEntity } from "../user/user.entity";
+import { CitiesEntity } from "../cities/cities.entity";
 
 import { CreateEventsDto } from "./dtos/create-events.dto";
 import { FilterEventsDto, QueryType } from "./dtos/events.dto";
