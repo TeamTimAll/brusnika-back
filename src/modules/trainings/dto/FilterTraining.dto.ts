c8832c2a205ae8bce0277678ea4feb3b399de702
MahdiyDev <mahdiydev@gmail.com>
2024-08-13 16:39:46 +0500

    Updated: filter training dto

--- src/modules/trainings/dto/FilterTraining.dto.ts
+++ src/modules/trainings/dto/FilterTraining.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsInt, IsNotEmpty } from "class-validator";
 
 export class FilterTrainingDto {
 	@ApiProperty({ required: false })
+	@Type(() => Number)
+	@IsInt()
+	@IsNotEmpty()
 	category_id?: number;
 }
