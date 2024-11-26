f268f1fc859143b7489b78d15e5a71c29c14a378
javohirbek <javokhirbek11@gmail.com>
2024-11-26 18:25:21 +0500

    fix bugs

--- src/modules/queues/visit/visit.service.ts
+++ src/modules/queues/visit/visit.service.ts
@@ -101,7 +101,7 @@ export class VisitQueueService {
 			data: {
 				requestType: "create_demo",
 				type: "offline",
-				date: `${visit.date.toISOString().split("T")[0]}T${visit.time}`,
+				date: `${new Date(visit.date).toISOString().split("T")[0]}T${visit.time}`,
 				name: client?.fullname,
 				phone: client?.phone_number,
 				project: project?.name,
