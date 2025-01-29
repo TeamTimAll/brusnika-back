af8e0e5934ea943ee6a0322fe1a176fd678f0816
Chemistry <22637787+Chemistry1026@users.noreply.github.com>
2025-01-29 15:51:04 +0500

    Debug 1c queue

--- src/modules/queues/queue.service.ts
+++ src/modules/queues/queue.service.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Injectable, Logger } from "@nestjs/common";
+import { HttpStatus, Injectable, Logger } from "@nestjs/common";
 import axios from "axios";
 
 import { BaseDto } from "../../common/base/base_dto";
 import { ConfigManager } from "../../config";
+import { BaseError } from "../../common/base/baseError";
+import { ApplicationPromptID } from "../../lib/prompt/applicationPrompt";
 
 @Injectable()
 export class QueueService {
@@ -24,19 +26,19 @@ export class QueueService {
 				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 				if (responseData.response && responseData.response.error) {
 					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-					throw new BadRequestException(responseData.response.error);
+					throw new BaseError(ApplicationPromptID.INTERNAL_SERVER_ERROR, { message: responseData.response.error }, HttpStatus.BAD_REQUEST);
 				}
 			}
 
 			return response;
 		} catch (error: any) {
-			console.log('1', error, error.message);
+			console.log('2', error.message);
 
-			if (error instanceof BadRequestException) {
+			if (error instanceof BaseError) {
 				throw error;
 			}
 
-			throw new BadRequestException(error.message);
+			throw new BaseError(ApplicationPromptID.INTERNAL_SERVER_ERROR, { message: error.message }, HttpStatus.BAD_REQUEST);
 		}
 	}
 }
